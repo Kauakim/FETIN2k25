18d243646531334b5814761699fbb557c8e102f9
Kauakim <kauasantarita@gmail.com>
2025-06-07 23:25:39 -0300

    feat: add the last base routes for FastAPI

---
+++ server/app/infos.py
@@ -0,0 +1,4 @@
+from .models import getInfoData, createInfo, updateInfo
+
+# Recebe as informações de cada uma das linhas de produção
+# Recebe as informações de cada um dos beacons
--- server/app/models.py
+++ server/app/models.py
@@ -17,102 +17,151 @@ def connectToDatabase():
 def getUsersData():
     connection = connectToDatabase()
     if connection is None:
-        return
+        return {}
     cursor = connection.cursor(dictionary=True)
-
-    query = "SELECT * FROM login"
-    cursor.execute(query)
-
-    users = cursor.fetchall()
-    usersData = {user["username"]: user for user in users}
-
-    cursor.close()
-    connection.close()
-
-    return usersData
+    try:
+        query = "SELECT * FROM login"
+        cursor.execute(query)
+        users = cursor.fetchall()
+        return {user["username"]: user for user in users}
+    except mysql.connector.Error as e:
+        print(f"Error fetching users data: {e}")
+        return {}
+    finally:
+        cursor.close()
+        connection.close()
 
 def createUser(username, password, email, admin):
     connection = connectToDatabase()
     if connection is None:
         return
     cursor = connection.cursor()
+    try:
+        query = "INSERT INTO login (username, password, email, admin) VALUES (%s, %s, %s, %s)"
+        values = (username, password, email, admin)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error creating user: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
 
-    query = "INSERT INTO login (username, password, email, admin) VALUES (%s, %s, %s, %s)"
-    values = (username, password, email, admin)
-    cursor.execute(query, values)
-
-    connection.commit()
-    cursor.close()
-    connection.close()
+def updateUser(oldUsername, newUsername, password, email, admin):
+    connection = connectToDatabase()
+    if connection is None:
+        return
+    cursor = connection.cursor()
+    try:
+        query = """
+        UPDATE login 
+        SET username= %s, password = %s, email = %s, admin = %s
+        WHERE username = %s
+        """
+        values = (newUsername, password, email, admin, oldUsername)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error updating user: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
 
 def getBeaconsData():
     connection = connectToDatabase()
     if connection is None:
-        return []
+        return {}
     cursor = connection.cursor(dictionary=True)
-
-    query = "SELECT * FROM beacons"
-    cursor.execute(query)
-
-    beacons = cursor.fetchall()
-    beaconsData = {id["id"]: id for id in beacons}
-
-    cursor.close()
-    connection.close()
-
-    return beaconsData
+    try:
+        query = "SELECT * FROM beacons"
+        cursor.execute(query)
+        beacons = cursor.fetchall()
+        return {beacon["id"]: beacon for beacon in beacons}
+    except mysql.connector.Error as e:
+        print(f"Error fetching beacons data: {e}")
+        return {}
+    finally:
+        cursor.close()
+        connection.close()
 
 def createBeacon(beacon, tipo, status, x, y):
     connection = connectToDatabase()
     if connection is None:
         return
     cursor = connection.cursor()
+    try:
+        query = "INSERT INTO beacons (beacon, tipo, status, x, y) VALUES (%s, %s, %s, %s, %s)"
+        values = (beacon, tipo, status, x, y)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error creating beacon: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
 
-    query = "INSERT INTO beacons (beacon, tipo, status, x, y) VALUES (%s, %s, %s, %s, %s)"
-    values = (beacon, tipo, status, x, y)
-    cursor.execute(query, values)
-
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-def getInfoData():
-    connection = connectToDatabase()
-    if connection is None:
-        return
-    cursor = connection.cursor(dictionary=True)
-
-    query = "SELECT * FROM info"
-    cursor.execute(query)
-
-    infos = cursor.fetchall()
-    infoData = {info["id"]: info for info in infos}
-
-    cursor.close()
-    connection.close()
-
-    return infoData
-
-def createInfo(linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas):
+def updateBeacon(beacon, tipo, status, x, y):
     connection = connectToDatabase()
     if connection is None:
         return
     cursor = connection.cursor()
+    try:
+        query = """
+        UPDATE beacons 
+        SET status = %s, x = %s, y = %s 
+        WHERE beacon = %s AND tipo = %s
+        """
+        values = (status, x, y, beacon, tipo)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error updating beacon: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
 
-    query = "INSERT INTO info (linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas) VALUES (%s, %s, %s, %s, %s, %s)"
-    values = (linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas)
-    cursor.execute(query, values)
+def getInfoData():
+    connection = connectToDatabase()
+    if connection is None:
+        return {}
+    cursor = connection.cursor(dictionary=True)
+    try:
+        query = "SELECT * FROM info"
+        cursor.execute(query)
+        infos = cursor.fetchall()
+        return {info["id"]: info for info in infos}
+    except mysql.connector.Error as e:
+        print(f"Error fetching info data: {e}")
+        return {}
+    finally:
+        cursor.close()
+        connection.close()
 
-    connection.commit()
-    cursor.close()
-    connection.close()
+def createInfo(linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas):
+    connection = connectToDatabase()
+    if connection is None:
+        return {}
+    cursor = connection.cursor(dictionary=True)
+    try:
+        query = "INSERT INTO info (linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error creating info data: {e}")
+    finally:
+        cursor.close()
+        connection.close()
 
 def updateInfo(linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, falhas):
     connection = connectToDatabase()
     if connection is None:
         return
     cursor = connection.cursor()
-    
     try:
         query = """
         UPDATE info 
@@ -121,14 +170,11 @@ def updateInfo(linha, maquina, numeroProdutos, horasTrabalhadas, rendimento, fal
         """
         values = (numeroProdutos, horasTrabalhadas, rendimento, falhas, linha, maquina)
         cursor.execute(query, values)
+        connection.commit()
     except mysql.connector.Error as e:
         print(f"Error updating info: {e}")
         connection.rollback()
-        cursor.close()
-        connection.close()
-        return
     finally:
-        connection.commit()
         cursor.close()
         connection.close()
 
@@ -137,56 +183,59 @@ def getRSSI(beacon, gateway):
     if connection is None:
         return
     cursor = connection.cursor(dictionary=True)
+    try:
+        column = f"rssi{gateway}"
+        query = f"SELECT {column} FROM gateways WHERE beacon = %s"
+        values = (beacon,)
+        cursor.execute(query, values)
+        result = cursor.fetchone()
+        if result:
+            return result[column]
+        else:
+            return None
+    except mysql.connector.Error as e:
+        print(f"Error fetching RSSI: {e}")
+        return None
+    finally:
+        cursor.close()
+        connection.close()
 
-    column = f"rssi{gateway}"
-    query = f"SELECT {column} FROM gateways WHERE beacon = %s"
-    values = (beacon)
-    cursor.execute(query, values)
-
-    result = cursor.fetchone()
-    cursor.close()
-    connection.close()
-
-    if result:
-        return result[f"rssi{gateway}"]
-    else:
+def createRSSI(beacon, rssi1, rssi2, rssi3, status):
+    connection = connectToDatabase()
+    if connection is None:
         return
+    cursor = connection.cursor()
+    try:
+        query = "INSERT INTO gateways (beacon, rssi1, rssi2, rssi3, status) VALUES (%s, %s, %s, %s, %s)"
+        values = (beacon, rssi1, rssi2, rssi3, status)
+        cursor.execute(query, values)
+        connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error updating beacon: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
+    return
 
 def updateRSSI(beacon, rssi, gateway):
     connection = connectToDatabase()
     if connection is None:
         return
     cursor = connection.cursor()
-
     try:
-        query = "UPDATE beacons SET rssi%s = %s WHERE beacon = %s"
+        query = "UPDATE gateways SET rssi%s = %s WHERE beacon = %s"
         values = (gateway, rssi, beacon)
         cursor.execute(query, values)
-    except mysql.connector.Error as e:
-        print(f"Error updating info: {e}")
-        connection.rollback()
-        cursor.close()
-        connection.close()
-        return
-    finally:
         connection.commit()
+    except mysql.connector.Error as e:
+        print(f"Error updating RSSI: {e}")
+        connection.rollback()
+    finally:
         cursor.close()
         connection.close()
 
 print(getUsersData())
 print(getBeaconsData())
 print(getInfoData())
-print(getRSSI("lote1", 1))
-
-'''
-createUser("admin", "admin123", "admin@gmail.com", True)
-createUser("user1", "user123", "user1@gmail.com", False)
-createUser("user2", "user123", "user2@gmail.com", False)
-createBeacon("lote1", "loteMacas", "100", 10.0, 20.0)
-createBeacon("lote2", "loteCaixas", "20", 0.0, 50.55)
-createBeacon("Chave de fenda", "ferramenta", "OK", 0.0, 0.0)
-createBeacon("Pinguim", "Funcionário", "Cagado", 100.0, 20.0)
-createInfo(1, 1, 100, 8, 95.5, 2)
-createInfo(1, 2, 150, 7, 90.0, 1)
-createInfo(2, 1, 200, 6, 85.0, 3)
-'''
+print(getRSSI(("lote1",), 1))
